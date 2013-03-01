2c693f0083a2bebd4af8919a107b5710812ffe3b
Tomi <tomi.syvajarvi@tut.fi>
2013-03-01 11:32:49 +0200

    Käyttöliittymälayoutit mainpager, serverlist ja nodebrowser

--- OpcUaSampleAppForAndroid/gen/com/prosysopc/ua/android/R.java
+++ OpcUaSampleAppForAndroid/gen/com/prosysopc/ua/android/R.java
@@ -10,27 +10,57 @@ package com.prosysopc.ua.android;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        /**  Default screen margins, per the Android Design guidelines. 
+
+         Customize dimensions originally defined in res/values/dimens.xml (such as
+         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
+    
+         */
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
+    }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int connectAndReadButton=0x7f050002;
-        public static final int serverTimeField=0x7f050001;
-        public static final int serverTimeLabel=0x7f050000;
+        public static final int LinearLayout1=0x7f07000c;
+        public static final int LinearLayout2=0x7f070006;
+        public static final int action_settings=0x7f07000d;
+        public static final int connectAndReadButton=0x7f070005;
+        public static final int fragment1=0x7f070009;
+        public static final int fragment2=0x7f07000a;
+        public static final int horizontalScrollView1=0x7f070008;
+        public static final int listView1=0x7f07000b;
+        public static final int pager=0x7f070000;
+        public static final int pager_title_strip=0x7f070001;
+        public static final int section_label=0x7f070002;
+        public static final int serverTimeField=0x7f070004;
+        public static final int serverTimeLabel=0x7f070003;
+        public static final int textView1=0x7f070007;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int activity_main_pager=0x7f030000;
+        public static final int fragment_main_pager_dummy=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int nodebrowser=0x7f030003;
+        public static final int nodelist_level_fragment=0x7f030004;
+        public static final int serverlist=0x7f030005;
+    }
+    public static final class menu {
+        public static final int main_pager=0x7f060000;
     }
     public static final class string {
-        public static final int action_settings=0x7f040005;
-        public static final int app_name=0x7f040000;
-        public static final int connect_and_read=0x7f040001;
-        public static final int init_time_string=0x7f040003;
-        public static final int time_on_server=0x7f040002;
-        public static final int title_activity_main=0x7f040004;
-        public static final int title_activity_server_settings=0x7f040009;
-        public static final int title_section1=0x7f040006;
-        public static final int title_section2=0x7f040007;
-        public static final int title_section3=0x7f040008;
+        public static final int action_settings=0x7f050005;
+        public static final int app_name=0x7f050000;
+        public static final int connect_and_read=0x7f050001;
+        public static final int init_time_string=0x7f050003;
+        public static final int time_on_server=0x7f050002;
+        public static final int title_activity_main=0x7f050004;
+        public static final int title_activity_main_pager=0x7f05000a;
+        public static final int title_activity_server_settings=0x7f050009;
+        public static final int title_section1=0x7f050006;
+        public static final int title_section2=0x7f050007;
+        public static final int title_section3=0x7f050008;
     }
 }
