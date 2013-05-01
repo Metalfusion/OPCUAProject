fd3f2c2225eb9fd43c3f9fcc2eecd0c282874856
Lempire <vk.lempinen@gmail.com>
2013-05-01 21:22:07 +0300

    Yhdistämistä aloitettu

--- OpcUaSampleAppForAndroid/src/com/prosysopc/ua/android/Connection.java
+++ OpcUaSampleAppForAndroid/src/com/prosysopc/ua/android/Connection.java
@@ -1,16 +1,31 @@
 package com.prosysopc.ua.android;
 
+
+
+import java.net.URISyntaxException;
+
+import com.prosysopc.ua.ServiceException;
+import com.prosysopc.ua.SessionActivationException;
+import com.prosysopc.ua.client.ConnectException;
+import com.prosysopc.ua.client.InvalidServerEndpointException;
+import com.prosysopc.ua.client.UaClient;
+
+
+
 public class Connection
 {
 	Server server;
-	public Connection( Server servervalue )
+	protected static UaClient client;
+	
+	public Connection( Server servervalue ) throws URISyntaxException
 	{
 		server = servervalue;
+		client = new UaClient( server.address );
 	}
 	
-	public boolean connect()
+	public boolean connect() throws InvalidServerEndpointException, ConnectException, SessionActivationException, ServiceException
 	{
-		
+		client.connect();
 		return true;
 	}
 }
